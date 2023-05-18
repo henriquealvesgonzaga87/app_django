95308b2d608d5e57937ec0cf29d066e3e3b1fc3f
Henrique <henriquealves87@hotmail.com>
2023-05-18 09:19:49 +0100

    this commit creates the table event and associate with user

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Event
 
-# Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("title", "event_date", "event_creation_date")
+    list_filter = ("user", "event_date",)
+
+
+admin.site.register(Event, EventAdmin)
